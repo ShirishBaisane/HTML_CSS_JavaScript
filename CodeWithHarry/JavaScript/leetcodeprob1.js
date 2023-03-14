065a6ed59aeab5cea643548de1d64c6c98e7dde5
Shirish Baisane <shirish.baisane@gmail.com>
2023-03-14 17:53:35 +0530

    JavaScript-w3resource: conditonals statement and loops done

--- CodeWithHarry/JavaScript/leetcodeprob1.js
+++ CodeWithHarry/JavaScript/leetcodeprob1.js
@@ -45,3 +45,88 @@ function defangIPaddr (address) {
 console.log(defangIPaddr(address1))
 console.log(defangIPaddr(address2))
 */
+
+// const users = {
+//     Alan: {
+//       online: false
+//     },
+//     Jeff: {
+//       online: true
+//     },
+//     Sarah: {
+//       online: false
+//     }
+//   }
+  
+//   function countOnline(usersObj) {
+//     // Only change code below this line
+//     let count=0
+//     for(let name in usersObj){
+//       console.log(name)
+//       console.log(usersObj[name].online)
+//       if(name['online']==true){
+//         count++
+//       }
+//     }
+//     return count;
+//     // Only change code above this line
+//   }
+  
+//   console.log(countOnline(users));
+
+
+// function largestOfFour(arr) {
+//     let largestNumberIndexI=0, largestNumberIndexJ=0, i=0, j=0
+//     while(i<arr.length){
+//       if(arr[i][j]>=arr[largestNumberIndexI][largestNumberIndexJ]){
+//         largestNumberIndexI=i;
+//         largestNumberIndexJ=j;
+//       }
+//       j++
+//       if(j==arr[i].length){
+//         i++
+//         j=0
+//       }
+//     }
+  
+//     return arr[largestNumberIndexI];
+//   }
+  
+//   largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+// function titleCase(str) {
+//     let arr = Array.from(str)
+
+//     for(let i=0; i<arr.length; i++){
+//       if(arr[i] != " "){
+//         arr[i] = arr[i].toUpperCase()
+//         console.log(arr[i])
+//         // i++
+//         while(arr[i+1] != " " && i+1<arr.length){
+//           i++;
+//           arr[i] = arr[i].toLowerCase();
+//           console.log(arr[i])
+//         }
+//       }
+//     }
+//     str = arr.join("")
+//     return str;
+//   }
+  
+//   console.log(titleCase("sHoRt AnD sToUt"));
+
+
+  function bouncer(arr) {
+    let resultArr = []
+    for(let i=0; i<arr.length; i++){
+      if(arr[i] != false && arr[i] != null && arr[i] != 0 && arr[i] != "" && arr[i] != undefined && arr[i] != NaN){
+        resultArr[resultArr.length] = arr[i]
+      }
+    }
+  
+    return resultArr;
+  
+  }
+  
+  console.log(bouncer([false, null, 0, NaN, undefined, ""]));
---
+++ CodeWithHarry/JavaScript/w3resource/1_loops_condStmt.js
@@ -0,0 +1,8 @@
+// 1. Write a JavaScript program that accept two integers and display the larger.
+
+const prompt = require("prompt-sync")()
+
+let int1 = prompt("Enter value 1: ")
+let int2 = prompt("Enter value 2: ")
+console.log("The value of integer 1 is: ",int1)
+console.log("The value of integer 2 is: ",int2)
