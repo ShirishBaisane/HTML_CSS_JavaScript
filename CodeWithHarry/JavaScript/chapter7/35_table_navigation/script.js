d530df451f624b689cb09f0bce350d35b6a0eda9
Shirish Baisane <shirish.baisane@gmail.com>
2023-03-16 19:40:00 +0530

    CodeWithHarry: JavaScript couse - Chapter6(Walking the DOM) 35-Table Navigation done

---
+++ CodeWithHarry/JavaScript/chapter7/35_table_navigation/script.js
@@ -0,0 +1,14 @@
+let t = document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.caption)
+console.log(t.rows)
+console.log(t.tHead)
+console.log(t.tHead.firstElementChild)
+// console.log(t.tFoot)
+console.log(t.tBodies)
+console.log(t.rows[1].rowIndex)
+// console.log(t.rows.rowIndex)
+console.log(t.rows[1].cells)
+
+
+// console.log(t)
